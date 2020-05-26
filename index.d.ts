f876c858c64eda83cb3c58ea03a34ac053d3b99f
Random bug <Fleuv@users.noreply.github.com>
2020-05-26 20:01:52 +0300

    feat(typescript): declarations (#77)

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+declare module 'initials' {
+	type NameOrNames = string | string[];
+
+	type ParsedNames = {name: string, initials: string};
+
+	interface Options {
+		existing: {
+			[name: string]: string
+		}
+	}
+
+	function initials(nameOrNames: NameOrNames, options?: Options): string | string[];
+
+	export function find(name: string): string;
+
+	export function parse(name: string): ParsedNames | undefined;
+
+	export function addTo(name: string): string | undefined;
+
+	export = initials;
+}
